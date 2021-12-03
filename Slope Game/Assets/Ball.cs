c6e133e609c4ffc73cb99ee748f4d6f1903a4c81
Aaron Murphy <aaron@checkventory.com>
2021-12-03 15:57:13 +0000

    material and procedural gen for tunnel

--- Slope Game/Assets/Ball.cs
+++ Slope Game/Assets/Ball.cs
@@ -27,6 +27,7 @@ public class Ball : MonoBehaviour {
   int x = 1;
   int z = 0;
  public int dif1 = 0;
+ public int speedup = 1;
 	void Start () {
 
 	}
@@ -68,7 +69,7 @@ public class Ball : MonoBehaviour {
             speed = speed + 10;
         }
         Debug.Log(score);
-      transform.position += Vector3.forward * Time.deltaTime * speed;
+        transform.position += Vector3.forward * Time.deltaTime * speed;
 
 
         //Detect if the left mouse button is pressed
@@ -87,7 +88,7 @@ public class Ball : MonoBehaviour {
             //transform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
             //transform.Translate(0,0, Input.GetAxis("Verical") * speed * Time.deltaTime);
         }
-        if (GameObject.Find("Sphere").transform.position.z>(256*i)-50){
+        if (GameObject.Find("Sphere").transform.position.z>(256*i)-100){
             /*
             //Create a new terrain
             GameObject parent = (GameObject)Instantiate(new GameObject("Terrain"));
@@ -101,8 +102,8 @@ public class Ball : MonoBehaviour {
             i++;
             */
             if (z == 0){
-                GameObject.Find("Terrain2").transform.position = new Vector3(0, 0, 256 * i - 50);
-                GameObject.Find("SpikedTerrain2").transform.position = new Vector3(-112, -200, 256 * i - 50);
+                GameObject.Find("Terrain2").transform.position = new Vector3(0, 0, 256 * i - 100);
+                GameObject.Find("SpikedTerrain2").transform.position = new Vector3(-112, -200, 256 * i - 100);
                 z++;
                 i++;
                 int min = 256*x;
@@ -115,12 +116,16 @@ public class Ball : MonoBehaviour {
                 GameObject.Find("Cube1").transform.position = new Vector3(10, 5, rand- 80);
                 rand = Random.Range(min, max);
                 GameObject.Find("Cube2").transform.position = new Vector3(10, 5, rand-120);
-
+                speedup++;
                 x++;
             }
             if (z==1){
-                GameObject.Find("Terrain1").transform.position = new Vector3(0, 0, 256 * i - 50);
-                  GameObject.Find("SpikedTerrain1").transform.position = new Vector3(-112, -200, 256 * i -50);
+                GameObject.Find("Terrain1").transform.position = new Vector3(0, 0, 256 * i - 100);
+                GameObject.Find("SpikedTerrain1").transform.position = new Vector3(-112, -200, 256 * i -100);
+                GameObject.Find("TunnelRight").transform.position = new Vector3(29, 5, 256 * i - 100);
+                GameObject.Find("TunnelLeft").transform.position = new Vector3(3, 5, 256 * i - 100);
+                GameObject.Find("TunnelTop").transform.position = new Vector3(15, 10, 256 * i - 100);
+
                 z++;
                 i++;
                 int min = 256*x;
@@ -133,12 +138,12 @@ public class Ball : MonoBehaviour {
                 GameObject.Find("Cube1").transform.position = new Vector3(10, 5, rand);
                 rand = Random.Range(min, max);
                 GameObject.Find("Cube2").transform.position = new Vector3(10, 5, rand);
-
+                speedup++;
                 x++;
             }
             if (z==2){
-                GameObject.Find("Terrain").transform.position = new Vector3(0, 0, 256 * i -50);
-                  GameObject.Find("SpikedTerrain").transform.position = new Vector3(-112, -200, 256 * i -50);
+                GameObject.Find("Terrain").transform.position = new Vector3(0, 0, 256 * i -100);
+                  GameObject.Find("SpikedTerrain").transform.position = new Vector3(-112, -200, 256 * i -100);
                 //Generate random num with min and max
                 int min = 256*x;
                 int max = 256*x+140;
@@ -149,17 +154,25 @@ public class Ball : MonoBehaviour {
                 GameObject.Find("Cube1").transform.position = new Vector3(20, 5, rand);
                 rand = Random.Range(min, max);
                 GameObject.Find("Cube2").transform.position = new Vector3(10, 5, rand);
-
+                speedup++;
                 z=0;
                 i++;
                 x++;
             }
 
+            //Collider
+            /*
+            Collider.OnCollisionEnter();
+              Collider.OnCollisionExit();
+              Collider.OnCollisionStay();
 
-            if (GameObject.Find("Sphere").transform.position.y < -100)
+              */
+
+
+            if (GameObject.Find("Sphere").transform.position.y < -10)
             {
                 // print Game Over
-                Application.LoadLevel(Application.loadedLevel);
+                //Application.LoadLevel(Application.loadedLevel);
                 Application.LoadLevel("GameOver");
 
 
