1ff5515ec0770750e860e5ede23994d4f87bed0c
Aaron Murphy <aaron@checkventory.com>
2021-12-08 15:08:19 +0000

    terrain generation

---
+++ Slope Game/Assets/TerrainGeneration.cs
@@ -0,0 +1,37 @@
+public class TerrainGeneration : MonoBehaviour
+{
+
+    public int width = 256;
+    public int height = 256;
+    public float scale = 20f;   
+    public float offsetX = 100f;
+    public float offsetY = 100f;
+    public float offsetZ = 100f;
+    public TerrainGeneration(){
+        void Start(){
+        }
+    }
+    void Update(){
+            if (GameObject.Find("Sphere").transform.position.z > (256*i)-100){
+                if (z==0){
+                    GameObject.Find("Terrain2").transform.position = new Vector3(0, 0, 256 * i - 100);
+                    GameObject.Find("SpikedTerrain2").transform.position = new Vector3(-167, -220, 256 * i - 100);
+                    z++;
+                    i++;
+                }
+                if (z==1){
+                    GameObject.Find("Terrain1").transform.position = new Vector3(0, 0, 256 * i - 100);
+                    GameObject.Find("SpikedTerrain1").transform.position = new Vector3(-167, -220, 256 * i - 100);
+                    z++;
+                    i++;
+                }
+                if (z==2){
+                    GameObject.Find("Terrain").transform.position = new Vector3(0, 0, 256 * i - 100);
+                    GameObject.Find("SpikedTerrain").transform.position = new Vector3(-167, -220, 256 * i - 100);
+                    z++;
+                    i++;
+                }
+        }
+        
+    }
+}
