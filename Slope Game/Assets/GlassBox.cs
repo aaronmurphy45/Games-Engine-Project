ae2dcf0b04e9420431706c9bf42b96d68dc01426
Aaron Murphy <aaron@checkventory.com>
2021-12-08 15:17:06 +0000

    galssbox / magic crate /crate

--- Slope Game/Assets/GlassBox.cs
+++ Slope Game/Assets/GlassBox.cs
@@ -1,8 +1,56 @@
+using UnityEngine;
 
-public class GlassBox : Item
-{
-    public GlassBox()
-    {
-       
+public class GlassBox : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "GlassBox1") {
+            if (checker==1){
+                        score = score + 10;
+                    }
+                    else{
+                        score = score - 50;
+                    }
+                    
+                    GameObject.Find("GlassBox1").transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+                    audio.Play();
+        }
+        if (other.gameObject.tag == "GlassBox2") {
+            if (checker==2){
+                        score = score + 10;
+                    }
+                    else{
+                        score = score - 50;
+                    }
+                    
+                    GameObject.Find("GlassBox2").transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+                    audio.Play();
+        }
+        if (other.gameObject.tag == "GlassBox3") {
+            if (checker==3){
+                        score = score + 10;
+                    }
+                    else{
+                        score = score - 50;
+                    }
+                    
+                    GameObject.Find("GlassBox3").transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+                    audio.Play();
+        }
+        if (other.gameObject.tag == "GlassBox4") {
+            if (checker==4){
+                        score = score + 10;
+                    }
+                    else{
+                        score = score - 50;
+                    }
+                    
+                    GameObject.Find("GlassBox4").transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+                    audio.Play();
+        }
+    }
+    private void Start() {
+        
+    }
+    private void Update() {
+        
     }
 }
--- Slope Game/Assets/MagicBox.cs
+++ Slope Game/Assets/MagicBox.cs
@@ -1,7 +1,23 @@
-public class MagicBox
-{
-    public MagicBox()
-    {
-        
-    }
+
+public class MagicBox : MonoBehaviour {
+    
+IEnumerator SpecialEffect(){
+     Material mat = Resources.Load("Special", typeof(Material)) as Material;
+        GameObject.Find("Sphere").GetComponent<MeshRenderer> ().material = mat;
+        score = score + 10;
+        GameObject.Find("Magic Box").transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        speed = 150;
+        checker = 1;
+        yield return new WaitForSecondsRealtime(5);
+        speed = 10;
+        checker = 0;
+        GameObject.Find("Sphere").GetComponent<MeshRenderer> ().material = Resources.Load("DefBall", typeof(Material)) as Material;
+}
+void Start () {
+    score = 0;
+    
+}
+void Update() {
+    
+}
 }
