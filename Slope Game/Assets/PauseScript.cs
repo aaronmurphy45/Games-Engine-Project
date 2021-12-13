1faa1e3b12460865c908fc3fdc7e40a10d0d3c5f
Aaron Murphy <aaron@checkventory.com>
2021-12-13 17:54:17 +0000

    Pause Menu Fix

--- Slope Game/Assets/PauseScript.cs
+++ Slope Game/Assets/PauseScript.cs
@@ -16,34 +16,39 @@ public class PauseScript : MonoBehaviour{
     void Update(){
         if (Input.GetKeyDown("p"))
         {
-            
-            
             if (GameIsPaused)
             {
-                pauseMenuUI.SetActive(false);
-                Time.timeScale = 1f;
-                GameIsPaused = false;
+                Resume();
             }
             else
             {
-                pauseMenuUI.SetActive(true);
-                Time.timeScale = 0f;
-                GameIsPaused = true;
+                Pause();
             }
         }
     }
-    /*
-    void Resume()
+    
+    public void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
-    void Pause()
+    public void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-    */
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Loading Menu");
+        Application.LoadLevel("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
 }
