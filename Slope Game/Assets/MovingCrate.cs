2eae456317a916d516d705a4ec7f534d3dca47c8
Aaron Murphy <aaron@checkventory.com>
2021-12-13 16:41:08 +0000

    deletion of uneccesaary files: TerrainData

--- Slope Game/Assets/MovingCrate.cs
+++ Slope Game/Assets/MovingCrate.cs
@@ -8,17 +8,18 @@ public class MovingCrate : MonoBehaviour {
 
     public bool going_right = false;
 
+    // This script is used to move the moving crate and also to play the sound effect and make the crate disappear when the crate is hit
+
    void OnTriggerEnter(Collider other) {
 
         
-        
+        // If the crate hits the player, the crate disappears and the sound effect is played
         if (other.gameObject.name == "Sphere") {
           
                 float posx = crate.transform.position.x;
                 float posz = crate.transform.position.z;
                 float posy = crate.transform.position.y;
 
-                //cracked.transform.position = new Vector3(posx, posy, posz + 20);
                 crate.transform.position = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
                 audioSource.clip = audio;
                 audioSource.Play();
@@ -38,7 +39,7 @@ public class MovingCrate : MonoBehaviour {
    void Update() {
        
        //move crate bewteen 2 points 
-
+        //get crate position
         float posx = crate.transform.position.x;
         float posz = crate.transform.position.z;
         float posy = crate.transform.position.y;
@@ -54,36 +55,13 @@ public class MovingCrate : MonoBehaviour {
             crate.transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
-
+        // if crate reaches end of plaform it will go the other way
         if (posx > 20) {
             going_right = false;
         }
         else if (posx < 0) {
             going_right = true;
         }
-
-
-
-
-
-/*
-        if (posx <= 0) {
-            Debug.Log("going right");
-            going_right = false;
-        }
-        if (posx >= 30) {
-            Debug.Log("here");
-            going_right = true;
-        }
-
-*/
-        
-        // 460 left
-
-        //490 right
-       
-        //crate.transform.position = Vector3.MoveTowards(crate.transform.position, new Vector3(crate.transform.position.x, crate.transform.position.y, crate.transform.position.z), speed * Time.deltaTime);
-        
 
 
    }
--- Slope Game/Assets/MovingCrateGeneration.cs
+++ Slope Game/Assets/MovingCrateGeneration.cs
@@ -4,12 +4,19 @@ public class MovingCrateGeneration : MonoBehaviour {
     public GameObject cube;
     public GameObject sphere;
     int i =1;
+
     
+    
+    // This script is used to procedurally generate the Moving Boxes.
 
         private void Update() {
+            // The length of each terrain is 256. 
+            // As the ball moves forward, the moving crate will be generated in a random position in bewteen that multiple of the length of the terrain.
             if (sphere.transform.position.z > (256*i)-30) {
+                 // Randomize x positon of the cube between 5 and 25 
                 int min = 5, max = 25;
                 int randx = Random.Range(min, max);
+                // Randomize z positon of the cube between 256 i and 256 i + 256
                 min = 256*i;
                 max = 256*i + 256;
                 int randz = Random.Range(min, max);
